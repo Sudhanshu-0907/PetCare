d1b2c16fb6e5d75978fc6e89e334ecc20f0b47fa
Sudhanshu-0907 <sudhanshusingh0907@gmail.com>
2024-09-02 02:29:22 +0530

    minor changes

--- App.js
+++ App.js
@@ -21,6 +21,7 @@ import {navigationRef} from './utils/RootNavigation';
 import {PaperProvider} from 'react-native-paper';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import messaging from '@react-native-firebase/messaging';
+import notifee from '@notifee/react-native';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -31,9 +32,34 @@ const store = createStore(
 sagaMiddleware.run(watch);
 
 const App = () => {
+  async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission();
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        // smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      onDisplayNotification();
     });
 
     return unsubscribe;
